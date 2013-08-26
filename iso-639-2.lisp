789464117e05ef558b17d53260fb54e9542652d1
Mark VandenBrink <mark@vandenbrink.us>
2013-08-26 12:21:08 -0500

    added mode line/copyright

--- iso-639-2.lisp
+++ iso-639-2.lisp
@@ -1,3 +1,6 @@
+;;; -*- Mode: Lisp;  show-trailing-whitespace: t; Base: 10; indent-tabs: nil; Syntax: ANSI-Common-Lisp; Package: ISO-639-2; -*-
+;;; Copyright (c) 2013, Mark VandenBrink. All rights reserved.
+
 (in-package #:iso-639-2)
 
 (defparameter *langs*
