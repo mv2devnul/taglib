689286a9e85d2efde857925fd97cd4ac65cb2f18
Mark VandenBrink <mark@vandenbrink.us>
2013-10-11 14:46:03 -0500

    added profile support

--- profile.lisp
+++ profile.lisp
@@ -1,5 +1,14 @@
+;;; -*- Mode: Lisp;  show-trailing-whitespace: t; Base: 10; indent-tabs: nil; Syntax: ANSI-Common-Lisp; Package: CL-USER; -*-
+;;; Copyright (c) 2013, Mark VandenBrink. All rights reserved.
+
+;;;;;;;;;;;;;;;;;;;; Handy, dandy CCL profile functions ;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Usage: load and compile this file, then at REPL, type "profile-on".  After
+;;; running programs, type "profile-report" to get a profile listing.
+;;; "profile-reset" clears counters
+;;; "profil-off" turns of profiling
 (defun profile-on ()
-  (dolist (p '("MP4-ATOM" "MPEG" "AUDIO-STREAMS" "ID3-FRAME" "UTILS" "LOGGING" "ISO-639-2" "MP3-TAG" "MP4-TAG"))
+  (dolist (p '("MP4-ATOM" "MPEG" "AUDIO-STREAMS" "ID3-FRAME" "UTILS" "LOGGING" "ISO-639-2" "MP3-TAG" "MP4-TAG" "FLAC-FRAME"))
     (let ((pkg (find-package p)))
       (mon:monitor-all pkg)
       (format t "Package ~a, ~:d~%" pkg (length mon:*monitored-functions*)))))
