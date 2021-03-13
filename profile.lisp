915f669dbb3e14f67e7ed79869e9a4ad2859f86a
Mark VandenBrink <mark@vandenbrink.us>
2021-03-12 20:46:15 -0600

    Updated name of profiler to swank-mon

--- profile.lisp
+++ profile.lisp
@@ -19,18 +19,18 @@
                 (cur-len 0))
             (dolist (p '("TREE" "MP4-ATOM" "MPEG" "AUDIO-STREAMS" "ID3-FRAME" "UTILS" "ISO-639-2" "ABSTRACT-TAG" "FLAC-FRAME"))
               (let ((pkg (find-package p)))
-                (mon:monitor-all pkg)
-                (setf cur-len (length mon:*monitored-functions*))
+                (swank-monitor:monitor-all pkg)
+                (setf cur-len (length swank-monitor:*monitored-functions*))
                 (format t "~4d functions/methods being monitored in package ~a~%" (- cur-len last-len) p)
                 (setf last-len cur-len)))
             (format t "~4d total functions/methods being monitored~%" last-len))
           (format t "~&~%~%~%~&WARNING: you MUST turn profiling off before recompiling library, or weirdness will ensue!!!~%~%"))
 
         (defun report (&key (sort-key :percent-time) (nested :exclusive))
-          (mon:report :sort-key sort-key :nested nested :threshold 0.0 :names :all))
+          (swank-monitor:report :sort-key sort-key :nested nested :threshold 0.0 :names :all))
 
         (defun reset ()
-          (mon:reset-all-monitoring))
+          (swank-monitor:reset-all-monitoring))
 
         (defun off ()
-          (mon:unmonitor)))
+          (swank-monitor:unmonitor)))
